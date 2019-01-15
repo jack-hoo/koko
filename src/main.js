538194307188eb806a49b5e70ccd4790deaa3d62
shugan <shugan@2dfire.com>
2019-01-15 14:46:05 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'vuetify/dist/vuetify.min.css'
+import 'babel-polyfill'
 
 Vue.config.productionTip = false
 
